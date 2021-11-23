c424088ad226a62492daf9c4f74a6570682fe486
YosstinCode <Yosstincode@gmail.com>
2021-11-23 14:28:23 -0500

    added listAlbum and album components

---
+++ src/app/components/list-albumes/list-albumes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-albumes',
+  templateUrl: './list-albumes.component.html',
+  styleUrls: ['./list-albumes.component.css']
+})
+export class ListAlbumesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
