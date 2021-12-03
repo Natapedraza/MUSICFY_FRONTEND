e0ced05312f8f4e282c401d2b47e19e853727b2f
YosstinCode <Yosstincode@gmail.com>
2021-12-03 08:27:59 -0500

    for deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/MUSICFY'),
+      dir: require('path').join(__dirname, './coverage/musicfy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/musicfy'));
+app.use(express.static("./dist/musicfy"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/musicfy/'}),
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/musicfy/" })
 );
 
 // Start the app by listening on the default Heroku port
